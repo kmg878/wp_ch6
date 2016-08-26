aefead3d0c194149c858be3dc49b71290c45628a
MinjeongKim <kmg878@naver.com>
2016-08-26 15:57:34 +0900

    2016-08-26

---
+++ WebContent/hello.js
@@ -0,0 +1,7 @@
+//window.location.href="http://www.sungkyul.ac.kr";
+document.write("<h1>Hello World</h1>");
+
+console.log("Hello World");
+console.error("Hellow World");
+console.info("Hellow World");
+console.warn("Hellow World");
